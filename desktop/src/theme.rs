1e5c60f76bd0f760d3c208ae07d3b79f00fdf941
Richard Custodio <richardcustodio@pm.me>
2024-02-08 20:35:29 -0300

    fix: remove unused `tier_color`

--- desktop/src/theme.rs
+++ desktop/src/theme.rs
@@ -96,21 +96,6 @@ pub fn expand_button() -> theme::Button {
     theme::Button::Custom(Box::new(Button::Expand))
 }
 
-pub fn tier_color(tier: crate::core::Tier) -> Color {
-    match tier {
-        crate::core::Tier::Iron(_) => Color::from_rgb8(87, 77, 79),
-        crate::core::Tier::Bronze(_) => Color::from_rgb8(140, 82, 58),
-        crate::core::Tier::Silver(_) => Color::from_rgb8(128, 152, 157),
-        crate::core::Tier::Gold(_) => Color::from_rgb8(205, 136, 55),
-        crate::core::Tier::Platinum(_) => Color::from_rgb8(78, 153, 150),
-        crate::core::Tier::Emerald(_) => Color::from_rgb8(20, 156, 58),
-        crate::core::Tier::Diamond(_) => Color::from_rgb8(87, 107, 206),
-        crate::core::Tier::Master(_) => Color::from_rgb8(157, 72, 224),
-        crate::core::Tier::Grandmaster(_) => Color::from_rgb8(205, 69, 69),
-        crate::core::Tier::Challenger(_) => Color::from_rgb8(244, 200, 116),
-    }
-}
-
 pub fn win_color(win: bool, remake: bool) -> Color {
     if remake {
         Color::from_rgb(0.8, 0.8, 0.8)
