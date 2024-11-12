f9ba4d4a231e130428a1b4e166c39d90b5907402
Richard Custodio <richardcustodio@pm.me>
2024-11-12 18:55:14 -0300

    refactor `ErrWrapper` into `error` module`

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,6 +1,4 @@
 use axum::extract::{Path, State};
-use axum::http::StatusCode;
-use axum::response;
 use axum::routing::get;
 use axum::{Json, Router};
 use futures::FutureExt;
@@ -19,6 +17,9 @@ use tracing_subscriber::fmt::time::UtcTime;
 use tracing_subscriber::prelude::*;
 use tracing_web::{performance_layer, MakeConsoleWriter};
 
+mod error;
+use error::Error;
+
 #[event(start)]
 fn start() {
     let fmt_layer = tracing_subscriber::fmt::layer()
@@ -33,40 +34,7 @@ fn start() {
         .init();
 }
 
-#[derive(Debug)]
-pub struct ErrWrapper(worker::Error);
-
-impl ErrWrapper {
-    fn from_string(value: impl ToString) -> Self {
-        ErrWrapper(worker::Error::RustError(value.to_string()))
-    }
-}
-
-impl std::fmt::Display for ErrWrapper {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl std::error::Error for ErrWrapper {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        self.0.source()
-    }
-}
-
-impl From<worker::Error> for ErrWrapper {
-    fn from(value: worker::Error) -> Self {
-        ErrWrapper(value)
-    }
-}
-
-impl response::IntoResponse for ErrWrapper {
-    fn into_response(self) -> response::Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
-    }
-}
-
-pub type Result<T> = std::result::Result<T, ErrWrapper>;
+pub type Result<T> = std::result::Result<T, Error>;
 
 fn router(env: Env) -> Result<Router> {
     let api_key = env.secret("RGAPI_KEY")?.to_string();
@@ -95,10 +63,10 @@ async fn fetch(
 ) -> Result<axum::http::Response<axum::body::Body>> {
     console_error_panic_hook::set_once();
     router(env)
-        .map_err(ErrWrapper::from_string)?
+        .map_err(Error::from_string)?
         .call(req)
         .await
-        .map_err(|_| ErrWrapper(worker::Error::Infallible))
+        .map_err(Error::infallible)
 }
 
 #[axum::debug_handler]
@@ -111,14 +79,9 @@ async fn fetch_summoner(
     let region = core::Region::from(region);
     let riot_id = name.replace("-", "#");
 
-    match kv
-        .get(&riot_id)
-        .text()
-        .await
-        .map_err(ErrWrapper::from_string)?
-    {
+    match kv.get(&riot_id).text().await.map_err(Error::from_string)? {
         Some(text) => {
-            let data = serde_json::from_str(&text).map_err(ErrWrapper::from_string)?;
+            let data = serde_json::from_str(&text).map_err(Error::from_string)?;
 
             tracing::debug!("returning cached summoner");
             return Ok(axum::Json(data));
@@ -128,7 +91,7 @@ async fn fetch_summoner(
 
     let Ok(summoner) = core::Summoner::from_name(client.clone(), riot_id.clone(), region).await
     else {
-        return Err(ErrWrapper::from_string("Summoner not found!"));
+        return Err(Error::from_string("Summoner not found!"));
     };
 
     let Ok(leagues) = summoner
@@ -136,14 +99,14 @@ async fn fetch_summoner(
         .await
         .map(|leagues| leagues.collect::<Vec<_>>())
     else {
-        return Err(ErrWrapper::from_string("Failed to fetch summoner leagues."));
+        return Err(Error::from_string("Failed to fetch summoner leagues."));
     };
 
     let mut games: Vec<core::Game> = stream::iter(summoner.matches(&client, 0..10, None).await)
         .flat_map(|game_ids| {
             stream::iter(game_ids).filter_map(|id| {
                 core::Game::from_id(&client, id)
-                    .map_err(ErrWrapper::from_string)
+                    .map_err(Error::from_string)
                     .map(Result::ok)
             })
         })
@@ -158,15 +121,15 @@ async fn fetch_summoner(
         games,
     };
 
-    let kv_data = serde_json::to_string(&data).map_err(ErrWrapper::from_string)?;
+    let kv_data = serde_json::to_string(&data).map_err(Error::from_string)?;
 
     kv.put(&riot_id, kv_data)
         .inspect_err(|e| tracing::error!("put failed: {e}"))
-        .map_err(ErrWrapper::from_string)?
+        .map_err(Error::from_string)?
         .execute()
         .await
         .inspect_err(|e| tracing::error!("execute failed: {e}"))
-        .map_err(ErrWrapper::from_string)?;
+        .map_err(Error::from_string)?;
 
     Ok(axum::Json(data))
 }
@@ -182,14 +145,9 @@ async fn fetch_matches(
 ) -> Result<Json<Matches>> {
     let client = core::Client::new(api_key);
 
-    match kv
-        .get(&puuid)
-        .text()
-        .await
-        .map_err(ErrWrapper::from_string)?
-    {
+    match kv.get(&puuid).text().await.map_err(Error::from_string)? {
         Some(text) => {
-            let matches = serde_json::from_str(&text).map_err(ErrWrapper::from_string)?;
+            let matches = serde_json::from_str(&text).map_err(Error::from_string)?;
 
             tracing::debug!("returning cached summoner");
             return Ok(axum::Json(matches));
@@ -202,7 +160,7 @@ async fn fetch_matches(
             .flat_map(|game_ids| {
                 stream::iter(game_ids).filter_map(|id| {
                     core::Game::from_id(&client, id)
-                        .map_err(ErrWrapper::from_string)
+                        .map_err(Error::from_string)
                         .map(Result::ok)
                 })
             })
@@ -213,15 +171,15 @@ async fn fetch_matches(
 
     let matches = Matches(games);
 
-    let kv_data = serde_json::to_string(&matches).map_err(ErrWrapper::from_string)?;
+    let kv_data = serde_json::to_string(&matches).map_err(Error::from_string)?;
 
     kv.put(&puuid, kv_data)
         .inspect_err(|e| tracing::error!("put failed: {e}"))
-        .map_err(ErrWrapper::from_string)?
+        .map_err(Error::from_string)?
         .execute()
         .await
         .inspect_err(|e| tracing::error!("execute failed: {e}"))
-        .map_err(ErrWrapper::from_string)?;
+        .map_err(Error::from_string)?;
 
     Ok(axum::Json(matches))
 }
